ec6f231407f00ab36ec383c464e534f9e1c79ae6
Ajay Viswanathan <ajayviswanathan@mediaiqdigital.com>
2018-05-22 19:05:29 +0530

    Define API for calculator

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "calculate"
+
+organization := "io.github.ajatix"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
---
+++ src/main/scala/io/github/ajatix/calculate/Expression.scala
@@ -0,0 +1,8 @@
+package io.github.ajatix.calculate
+
+sealed trait Expression
+case class Number(x: Int) extends Expression
+case class Add(x: Expression, y: Expression) extends Expression
+case class Subtract(x: Expression, y: Expression) extends Expression
+case class Multiply(x: Expression, y: Expression) extends Expression
+case class Divide(x: Expression, y: Expression) extends Expression
