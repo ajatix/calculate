a96494858b86e53c799b1d894a8f2a953ed74253
Ajay Viswanathan <ajayviswanathan@mediaiqdigital.com>
2018-05-22 19:38:29 +0530

    Implement cost function

--- src/main/scala/io/github/ajatix/calculate/Expression.scala
+++ src/main/scala/io/github/ajatix/calculate/Expression.scala
@@ -9,10 +9,10 @@ case class Divide(x: Expression, y: Expression) extends Expression
 
 object Expression {
 
-  def add(x: Number, y: Number): Number = Number(x.n + y.n)
-  def subtract(x: Number, y: Number): Number = Number(x.n - y.n)
-  def multiply(x: Number, y: Number): Number = Number(x.n * y.n)
-  def divide(x: Number, y: Number): Number = Number(x.n / y.n)
+  private def add(x: Number, y: Number): Number = Number(x.n + y.n)
+  private def subtract(x: Number, y: Number): Number = Number(x.n - y.n)
+  private def multiply(x: Number, y: Number): Number = Number(x.n * y.n)
+  private def divide(x: Number, y: Number): Number = Number(x.n / y.n)
 
   def evaluate(e: Expression): Number = e match {
     case n: Number => n
@@ -22,12 +22,21 @@ object Expression {
     case Divide(x, y) => divide(evaluate(x), evaluate(y))
   }
 
+  def calculateCost(e: Expression, agg: Int = 0): Int = e match {
+    case n: Number => 0
+    case Add(x, y) => agg + calculateCost(x) + calculateCost(y) + 1
+    case Subtract(x, y) => agg + calculateCost(x) + calculateCost(y) + 1
+    case Multiply(x, y) => agg + calculateCost(x) + calculateCost(y) + 1
+    case Divide(x, y) => agg + calculateCost(x) + calculateCost(y) + 1
+  }
+
 }
 
 object ExpressionDSL {
-  import Expression.{evaluate => eval}
+  import Expression.{evaluate => eval, _}
 
   implicit class RichExpression(e: Expression) {
     def evaluate(): Number = eval(e)
+    def cost(): Int = calculateCost(e)
   }
 }
